21dd7f8885f5992022863619b7694d9e02f0bd30
Dawit Abera <dawit.y.abera@nordstrom.com>
2019-12-27 00:43:43 -0800

    Added DB connection and proper error handling

--- server.js
+++ server.js
@@ -5,8 +5,12 @@ const morgan = require('morgan');
 // Router files
 const stores = require('./routes/stores');
 
+const connectDB = require('./config/db');
+
 // Load ENV Variables
 dotenv.config({ path: './config/config.env' });
+// Connect to database
+connectDB();
 
 const app = express();
 
@@ -19,7 +23,16 @@ if (process.env.NODE_ENV === 'development') {
 app.use('/api/v1/stores', stores);
 const PORT = process.env.PORT;
 
-app.listen(
+const server = app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
+
+// Handle all promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  // close server and exit process
+  server.close(() => {
+    process.exit(1);
+  });
+});
