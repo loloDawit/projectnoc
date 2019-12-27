69d62cce4946220c67c50c6050b00b56c2d3d2f8
Dawit Abera <dawit.y.abera@nordstrom.com>
2019-12-26 22:29:37 -0800

    Added router and to components

--- server.js
+++ server.js
@@ -2,11 +2,18 @@
 const express = require('express');
 const dotenv = require('dotenv');
 
+// Router files
+const stores = require('./routes/stores');
+
 // Load ENV Variables
-dotenv.config({path: './config/config.env'});
+dotenv.config({ path: './config/config.env' });
 
 const app = express();
-
+// Mount routers
+app.use('/api/v1/stores', stores);
 const PORT = process.env.PORT;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
