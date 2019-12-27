924df7a803b02a74409a77dc378d6c0817a3a85f
Dawit Abera <dawit.y.abera@nordstrom.com>
2019-12-27 14:10:57 -0800

    Store schema added

---
+++ models/Store.js
@@ -0,0 +1,90 @@
+const mongoose = require('mongoose');
+
+const StoreSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please add a name'],
+    unique: true,
+    trim: true,
+    maxlength: [50, 'Name can not be more than 50 characters']
+  },
+  slug: String,
+  description: {
+    type: String,
+    required: [true, 'Please add a name'],
+    maxlength: [500, 'Discription can not be more than 500 characters']
+  },
+  website: {
+    type: String,
+    match: [
+      /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
+      'Please use a valid URL'
+    ]
+  },
+  phone: {
+    type: String,
+    maxlength: [20, 'Phone number can be longer than 20 characters']
+  },
+  email: {
+    type: String,
+    match: [
+      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      'Please use a valid email'
+    ]
+  },
+  address: {
+    type: String,
+    required: [true, 'Please add address']
+  },
+  location: {
+    type: {
+      type: String,
+      enum: ['Point'],
+      required: true
+    },
+    coordinates: {
+      type: [Number],
+      required: true,
+      index: '2dsphere'
+    },
+    formattedAddress: String,
+    street: String,
+    city: String,
+    state: String,
+    zipcode: String,
+    country: String
+  },
+  engineers: {
+    type: [String],
+    required: true,
+    enum: [
+      'Mobile Developer',
+      'Backend Developer',
+      'Frontend Developer',
+      'Network Engineer',
+      'Data Scientist',
+      'Store Admin',
+      'Other'
+    ]
+  },
+  averageRating: {
+    type: Number,
+    min: [1, 'Rating must be at least 1'],
+    max: [10, 'Rating must not be more than 10']
+  },
+  averageRevenue: Number,
+  photo: {
+    type: String,
+    default: 'no-photo.jpg'
+  },
+  establishedat: {
+    type: Date,
+    default: Date.now
+  },
+  isFlagship: {
+    type: Boolean,
+    default: false
+  }
+});
+
+module.exports = mongoose.model('Store', StoreSchema);
