553a9935a86d2fcaa76adf367e0a38eee5e98443
Dawit Abera <dawit.y.abera@nordstrom.com>
2019-12-29 09:47:27 -0800

    Added getProject and getAllProjects function

--- controllers/projects.js
+++ controllers/projects.js
@@ -2,10 +2,14 @@ const Project = require('../models/Project');
 const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
 
-// @desc        GET ProjectS
-// @route       GET /api/v1/projects
-// @route       GET /api/v1/stores/:storeId/projects
-// @access      Public
+/**
+ **  getProjects
+ *
+ * * Description Get all projects
+ * * Route       GET /api/v1/projects
+ * * Route       GET /api/v1/stores/:storeId/projects
+ * * Access      Public
+ */
 exports.getProjects = asyncHandler(async (req, res, next) => {
   let query;
   if (req.params.storeId) {
@@ -24,9 +28,37 @@ exports.getProjects = asyncHandler(async (req, res, next) => {
     data: projects
   });
 });
-// @desc        Create new Project
-// @route       POST /api/v1/project
-// @access      Private
+/**
+ **  getProject
+ *
+ * * Description Get a project using the project id
+ * * Route       GET /api/v1/projects/:id
+ * * Access      Public
+ */
+exports.getProject = asyncHandler(async (req, res, next) => {
+  console.log(req.params.id);
+
+  const project = await Project.findById(req.params.id).populate({
+    path: 'store',
+    select: 'name description'
+  });
+  if (!project) {
+    return next(
+      ErrorResponse(`Project not found with the id of ${req.params.id}`, 404)
+    );
+  }
+  res.status(200).json({
+    success: true,
+    data: project
+  });
+});
+/**
+ **  createProject
+ *
+ * * Description Create a new Project
+ * * Route       POST /api/v1/project
+ * * Access      Private
+ */
 exports.createProject = asyncHandler(async (req, res, next) => {
   const project = await Project.create(req.body);
 
@@ -35,3 +67,6 @@ exports.createProject = asyncHandler(async (req, res, next) => {
     data: project
   });
 });
+// @desc        Get a Single Project
+// @route       Get /api/v1/stores/:id
+// @access      Public
