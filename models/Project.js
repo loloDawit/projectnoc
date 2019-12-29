4f1869432b0bac38844b2da9f2e9dd4bfb39459a
Dawit Abera <dawit.y.abera@nordstrom.com>
2019-12-29 13:21:09 -0800

    Added a middle ware to caculate average cost

--- models/Project.js
+++ models/Project.js
@@ -38,5 +38,42 @@ const ProjectSchema = mongoose.Schema({
     required: true
   }
 });
-
+/**
+ * *    Middleware to calculate average project cost
+ */
+ProjectSchema.statics.getAverageProjectCost = async function(storeId) {
+  const objectId = await this.aggregate([
+    //create pipelines
+    {
+      $match: { store: storeId }
+    },
+    {
+      $group: {
+        _id: '$store',
+        averageCost: { $avg: '$budget' }
+      }
+    }
+  ]);
+  try {
+    await this.model('Store').findByIdAndUpdate(storeId, {
+      averageCost: Math.ceil(objectId[0].averageCost / 10) * 10
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+/**
+ * TODO Calculate project cost
+ * ! After saving data
+ */
+ProjectSchema.post('save', function() {
+  this.constructor.getAverageProjectCost(this.store);
+});
+/**
+ * TODO Calculate project cost
+ * ! Before removing data
+ */
+ProjectSchema.pre('save', function() {
+  this.constructor.getAverageProjectCost(this.store);
+});
 module.exports = mongoose.model('Project', ProjectSchema);
