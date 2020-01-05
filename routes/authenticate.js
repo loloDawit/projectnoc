60c2035ec14a06a5748615edb81a5cc0a3a69292
Dawit Abera <dawit.y.abera@nordstrom.com>
2020-01-05 09:06:03 -0800

    Added route protection

--- routes/authenticate.js
+++ routes/authenticate.js
@@ -1,9 +1,13 @@
 const express = require('express');
-const { registerUser, signInUser } = require('../controllers/authenticate');
-
+const {
+  registerUser,
+  signInUser,
+  getAdmin
+} = require('../controllers/authenticate');
+const { protect } = require('../middleware/auth');
 const router = express.Router();
 
 router.post('/register', registerUser);
 router.post('/authenticate/signin', signInUser);
-
+router.get('/admin', protect, getAdmin);
 module.exports = router;
