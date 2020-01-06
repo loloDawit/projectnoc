1e1ab2c3671b47832bd25d8d1bf305c496a36aba
Dawit Abera <dawit.y.abera@nordstrom.com>
2020-01-06 13:58:41 -0800

    added new func

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -70,7 +70,7 @@ exports.createReview = asyncHandler(async (req, res, next) => {
   }
   const review = await Review.create(req.body);
 
-  res.status(200).json({
+  res.status(201).json({
     success: true,
     data: review
   });
